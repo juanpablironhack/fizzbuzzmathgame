431dc24a6606ad9442222ca60b3f3cdd9f731500
vagrant <vagrant@ironhack>
2014-10-21 15:09:11 +0000

    Cicle to travel conditions created

--- exercice2.rb
+++ exercice2.rb
@@ -1,23 +1,31 @@
-def game(alumns)
-	if (alumns%3==0 && alumns%5==0 && alumns<=100)
-		alumns="FizzBuzz,"
-		puts alumns
-		alumns+=1
-	end
-	if alumns%3==0 && alumns%5!=0  && alumns<=100
-		alumns="Fizz,"
-		puts alumns
-		alumns+=1
-	end
-	if alumns%3!=0 && alumns%5==0  && alumns<=100
-		alumns="Buzz,"
-		puts alumns
-		alumns+=1
-	end
-	if alumns%3!=0 && alumns%5!=0  && alumns<=100
-		puts "#{alumns},"
-		alumns+=1
-	end
+=begin
+for i in 0..100
+game (i)
 end
-game (1)
-
+=end
+$i = 0
+$num = 100
+while $i < $num  do
+	def game(alumns)
+		if (alumns%3==0 && alumns%5==0 && alumns<=100)
+			alumns="FizzBuzz,"
+			puts alumns
+			alumns+=1
+		end
+		if alumns%3==0 && alumns%5!=0  && alumns<=100
+			alumns="Fizz,"
+			puts alumns
+			alumns+=1
+		end
+		if alumns%3!=0 && alumns%5==0  && alumns<=100
+			alumns="Buzz,"
+			puts alumns
+			alumns+=1
+		end
+		if alumns%3!=0 && alumns%5!=0  && alumns<=100
+			puts "#{alumns},"
+			alumns+=1
+		end
+	end
+   $i +=1
+end
