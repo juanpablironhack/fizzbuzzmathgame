e602154bbadbebe015d089208075050fd71209a8
vagrant <vagrant@ironhack>
2014-10-22 15:21:12 +0000

    Exercice finished

--- exercice2.rb
+++ exercice2.rb
@@ -1,31 +1,20 @@
-=begin
-for i in 0..100
-game (i)
-end
-=end
-$i = 0
-$num = 100
-while $i < $num  do
-	def game(alumns)
-		if (alumns%3==0 && alumns%5==0 && alumns<=100)
-			alumns="FizzBuzz,"
-			puts alumns
-			alumns+=1
-		end
-		if alumns%3==0 && alumns%5!=0  && alumns<=100
-			alumns="Fizz,"
-			puts alumns
-			alumns+=1
-		end
-		if alumns%3!=0 && alumns%5==0  && alumns<=100
-			alumns="Buzz,"
-			puts alumns
-			alumns+=1
-		end
-		if alumns%3!=0 && alumns%5!=0  && alumns<=100
-			puts "#{alumns},"
-			alumns+=1
-		end
+def game(alumns)
+	if alumns%3==0 && alumns%5==0
+		puts "FizzBuzz,"
 	end
-   $i +=1
+	if alumns%3==0 && alumns%5!=0  && alumns<=100
+		puts "Fizz,"
+	end
+	if alumns%3!=0 && alumns%5==0  && alumns<=100
+		puts "Buzz,"
+	end
+	if alumns%3!=0 && alumns%5!=0  && alumns<=100
+		puts "#{alumns},"
+	end
+end
+alumns = 0
+$num = 100
+while alumns < $num  do
+	game(alumns)
+   alumns +=1
 end
